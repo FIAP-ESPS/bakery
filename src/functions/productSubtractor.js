82d20cadc194becb4ccb8adbbcfc1d3beb30698b
Eduardo F <eduardofidelis9@gmail.com>
2023-10-25 21:32:14 -0300

    feat: product subtractor

--- src/functions/productSubtractor.js
+++ src/functions/productSubtractor.js
@@ -1,9 +1,20 @@
 const prompt = require('prompt-sync')();
 
-const { displayProducts } = require('../displays/displayProducts');
+const { displayProducts } = require("../displays/displayProducts");
 
 function productSubtractor(availableProducts) {
   displayProducts(availableProducts);
 
-  const removedProduct = prompt('Qual produto você gostaria de remover do nosso estoque? ');
+  const removedProduct = Number(prompt(
+    '\nQual produto você gostaria de remover do nosso estoque? '
+  ));
+
+  console.log(`\nO produto ${availableProducts[
+    removedProduct
+  ].name.toLowerCase()} foi removido`)
+
+  availableProducts.splice(removedProduct, 1);
+
+  
 }
+module.exports = { productSubtractor };
--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
-const { start } = require("./functions/start.js");
-const { chooseOption } = require("./choices/chooseOption.js");
-const { buyProducts } = require("./functions/buyProducts.js");
-const { salesReport } = require("./functions/salesReport.js");
-const { productAdder } = require("./functions/productAdder.js");
 
-let availableProducts = [];
+const { start } = require('./functions/start');
+const { chooseOption } = require('./choices/chooseOption');
+const { buyProducts } = require('./functions/buyProducts');
+const { salesReport } = require('./functions/salesReport');
+const { productAdder } = require('./functions/productAdder');
+const { productSubtractor } = require('./functions/productSubtractor');
+
+const availableProducts = [];
 let chosenOption;
 let chosenProduct;
 let chosenQuantity;
 let chosenReport;
-let unitiesSold = [];
-let itemsSold = [];
-let valueSold = [];
+const unitiesSold = [];
+const itemsSold = [];
+const valueSold = [];
 
 class Product {
-  name
-  price
-  quantity
+  name;
+  price;
+  quantity;
 
-  constructor(productName, productPrice, productQuantity){
+  constructor(productName, productPrice, productQuantity) {
     this.name = productName;
     this.price = productPrice;
     this.quantity = productQuantity;
   }
 }
-let Pão = new Product('Pão', 1, 300);
-let Bolo = new Product('Bolo', 10, 45);
-let Queijo = new Product('Queijo', 7, 90);
+const Pão = new Product('Pão', 1, 300);
+const Bolo = new Product('Bolo', 10, 45);
+const Queijo = new Product('Queijo', 7, 90);
 
 availableProducts.push(Pão, Bolo, Queijo);
 
 loop: while (1) {
   start();
   switch (chooseOption(chosenOption)) {
-    case "0":
+    case '0':
       buyProducts(
         availableProducts,
         chosenProduct,
@@ -43,20 +45,20 @@ loop: while (1) {
         valueSold,
       );
       continue;
-    case "1":
-      productEditor()
+    case '1':
+      productEditor();
       continue;
-    case "2":
-      productSubtractor()
-      continue
-    case "3":
+    case '2':
+      productSubtractor(availableProducts);
+      continue;
+    case '3':
       productAdder(availableProducts, Product);
       continue;
-    case "4":
+    case '4':
       salesReport(chosenReport, unitiesSold, itemsSold, valueSold);
-      continue
-    case "5":
-      console.log("\nObrigado pela preferência e volte sempre!");
+      continue;
+    case '5':
+      console.log('\nObrigado pela preferência e volte sempre!');
       break loop;
   }
 }
